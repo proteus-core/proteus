3b68336ecab46f429725df0aff323954cbe5f13b
Job Noorman <job.noorman@cs.kuleuven.be>
2019-04-09 16:09:08 +0200

    Implement the marchid CSR

--- src/main/scala/riscv/plugins/MachineMode.scala
+++ src/main/scala/riscv/plugins/MachineMode.scala
@@ -41,11 +41,18 @@ private class Mvendorid(implicit config: Config) extends Csr {
   override def read(): UInt = U(0, config.xlen bits)
 }
 
+private class Marchid(implicit config: Config) extends Csr {
+  // Implement if we ever apply for an open-source machine architecture ID with
+  // the RISC-V foundation.
+  override def read(): UInt = U(0, config.xlen bits)
+}
+
 class MachineMode(implicit config: Config) extends Plugin {
   override def setup(pipeline: Pipeline): Unit = {
     val csr = pipeline.getService[CsrService]
 
     csr.registerCsr(pipeline, 0xF11, new Mvendorid)
+    csr.registerCsr(pipeline, 0xF12, new Marchid)
     csr.registerCsr(pipeline, 0x301, new Misa)
   }
 }
