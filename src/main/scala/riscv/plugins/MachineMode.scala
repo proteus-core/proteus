96e594fb5079fac8056f199fd1157f15aa3fbc83
Job Noorman <job.noorman@cs.kuleuven.be>
2019-04-09 15:58:30 +0200

    Move the definition of the misa CSR to a top-level class
    Instead of adding all the logic in an anonymous class in the setup()
    method.

--- src/main/scala/riscv/plugins/MachineMode.scala
+++ src/main/scala/riscv/plugins/MachineMode.scala
@@ -4,40 +4,42 @@ import riscv._
 
 import spinal.core._
 
+private class Misa(implicit config: Config) extends Csr {
+  val mxlVal = config.xlen match {
+    case 32 => 1
+    case 64 => 2
+  }
+
+  val mxl = U(mxlVal, 2 bits)
+  val extensions = U(0, 26 bits)
+
+  val baseExtension = config.baseIsa match {
+    case BaseIsa.RV32E => 'E'
+    case _ => 'I'
+  }
+
+  val supportedExtensions = Seq(baseExtension)
+
+  for (extension <- supportedExtensions) {
+    extensions(extension - 'A') := True
+  }
+
+  val filling = U(0, config.xlen - 28 bits)
+
+  val misa = mxl ## filling ## extensions
+
+  override def read(): UInt = {
+    misa.asUInt
+  }
+
+  // Writes are ignored
+  override def write(value: UInt): Unit = ()
+}
+
 class MachineMode(implicit config: Config) extends Plugin {
   override def setup(pipeline: Pipeline): Unit = {
     val csr = pipeline.getService[CsrService]
 
-    csr.registerCsr(pipeline, 0x301, new Csr {
-      private val mxlVal = config.xlen match {
-        case 32 => 1
-        case 64 => 2
-      }
-
-      private val mxl = U(mxlVal, 2 bits)
-      private val extensions = U(0, 26 bits)
-
-      private val baseExtension = config.baseIsa match {
-        case BaseIsa.RV32E => 'E'
-        case _ => 'I'
-      }
-
-      private val supportedExtensions = Seq(baseExtension)
-
-      for (extension <- supportedExtensions) {
-        extensions(extension - 'A') := True
-      }
-
-      private val filling = U(0, config.xlen - 28 bits)
-
-      private val misa = mxl ## filling ## extensions
-
-      override def read(): UInt = {
-        misa.asUInt
-      }
-
-      // Writes are ignored
-      override def write(value: UInt): Unit = ()
-    })
+    csr.registerCsr(pipeline, 0x301, new Misa)
   }
 }
