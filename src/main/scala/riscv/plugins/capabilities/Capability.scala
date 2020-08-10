f4949e4117cbc3303025551a3ad0b1037a64159c
Job Noorman <job@noorman.info>
2020-08-10 12:45:55 +0200

    Create Bundle to represent capabilities without offset
    Sometimes, most notably the PCC register, capabilities are stored
    without an offset. To make sure no register space is wasted, a separate
    Bundle class called NonPointerCapability is created to represent such
    capabilities.

--- src/main/scala/riscv/plugins/capabilities/Capability.scala
+++ src/main/scala/riscv/plugins/capabilities/Capability.scala
@@ -20,18 +20,33 @@ case class Permissions() extends Bundle {
   }
 }
 
-case class Capability(implicit context: Context) extends Bundle {
+class BaseCapability(implicit context: Context) extends Bundle {
   val tag = Bool
 
   private val xlen = context.config.xlen
   val base = UInt(xlen bits)
-  val offset = UInt(xlen bits)
   val length = UInt(xlen bits)
 
   val perms = Permissions()
 
-  def address = base + offset
   def top = base + length
+
+  def :=(other: Capability): Unit = {
+    assignAllByName(other)
+  }
+}
+
+case class NonPointerCapability(implicit context: Context) extends BaseCapability
+
+case class Capability(implicit context: Context) extends BaseCapability {
+  val offset = UInt(context.config.xlen bits)
+
+  def address = base + offset
+
+  def assignFrom(base: BaseCapability, offset: UInt) = {
+    assignSomeByName(base)
+    this.offset := offset
+  }
 }
 
 object Capability {
