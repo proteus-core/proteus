a6fd28ac679ad70cb95154795163e57565eb82e5
Job Noorman <job@noorman.info>
2020-08-18 11:56:49 +0200

    Add a CHERI core with external memory
    To be able to generate a Verilator simulator.

--- src/main/scala/riscv/plugins/capabilities/Core.scala
+++ src/main/scala/riscv/plugins/capabilities/Core.scala
@@ -9,17 +9,27 @@ import spinal.core._
 import spinal.lib._
 import spinal.core.sim._
 
+object createCheriPipeline {
+  def apply()(implicit config: Config): Pipeline = {
+    val pipeline = createStaticPipeline(build = false)
+
+    implicit val context = Context(pipeline)
+    pipeline.addPlugins(Seq(
+      new RegisterFile(pipeline.stages(1), pipeline.stages.last),
+      new Access(pipeline.stages(2)),
+      new ScrFile(pipeline.stages.last),
+      new Lsu,
+      new ExceptionHandler
+    ))
+
+    pipeline
+  }
+}
+
 class Core(imemHexPath: String) extends Component {
   implicit val config = new Config(BaseIsa.RV32I)
-  val pipeline = createStaticPipeline(build = false)
 
-  implicit val context = Context(pipeline)
-  pipeline.addPlugins(Seq(
-    new RegisterFile(pipeline.stages(1), pipeline.stages.last),
-    new Access(pipeline.stages(2)),
-    new ScrFile(pipeline.stages.last)
-  ))
-
+  val pipeline = createCheriPipeline()
   pipeline.build()
 
   val charDev = new CharDev
@@ -74,5 +84,46 @@ object CoreSim {
         byteDevSim.eval()
       }
     }
+  }
+}
+
+class CoreExtMem extends Component {
+  setDefinitionName("Core")
+
+  implicit val config = new Config(BaseIsa.RV32I)
+  val pipeline = createCheriPipeline()
+  pipeline.build()
+
+  val ibus = master(new MemBus(config.ibusConfig))
+  val dbus = master(new MemBus(config.dbusConfig))
+
+  val charDev = new CharDev
+  val charOut = master(Flow(UInt(8 bits)))
+  charOut << charDev.io
+
+  val testDev = new TestDev
+  val testOut = master(Flow(UInt(config.xlen bits)))
+  testOut << testDev.io
+
+  val byteDev = new ByteDev
+  val byteIo = master(new ByteDevIo)
+  byteIo <> byteDev.io
+  byteDev.irq <> pipeline.getService[InterruptService].getExternalIrqIo
+
+  val soc = new Soc(
+    pipeline,
+    Seq(
+      MemBusSegment(0x0, 10 MiB, dbus, ibus),
+      MmioSegment(0xf0001000L, new MachineTimers(pipeline)),
+      MmioSegment(0xf0002000L, charDev),
+      MmioSegment(0xf0003000L, testDev),
+      MmioSegment(0xf0004000L, byteDev)
+    )
+  )
+}
+
+object CoreExtMem {
+  def main(args: Array[String]) {
+    SpinalVerilog(new CoreExtMem)
   }
 }
