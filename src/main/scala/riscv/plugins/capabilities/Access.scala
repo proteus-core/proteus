9c1c37c695336b6563d639db8c364f37a9a6e435
Job Noorman <job@noorman.info>
2020-08-21 11:21:02 +0200

    Don't output values when the pipeline is stalled in Access.scala

--- src/main/scala/riscv/plugins/capabilities/Access.scala
+++ src/main/scala/riscv/plugins/capabilities/Access.scala
@@ -63,47 +63,56 @@ class Access(stage: Stage)(implicit context: Context) extends Plugin[Pipeline] {
 
       when (arbitration.isValid) {
         when (value(Data.CGET)) {
-          stage.arbitration.rs1Needed := True
+          arbitration.rs1Needed := True
           val cap = value(context.data.CS1_DATA)
-          val rd = value(Data.CFIELD).mux(
-            FieldSelect.PERM   -> cap.perms.asIsaBits.asUInt.resized,
-            FieldSelect.BASE   -> cap.base,
-            FieldSelect.LEN    -> cap.length,
-            FieldSelect.TAG    -> cap.tag.asUInt.resized,
-            FieldSelect.OFFSET -> cap.offset,
-            FieldSelect.ADDR   -> (cap.base + cap.offset) // TODO: use ALU
-          )
 
-          output(pipeline.data.RD_DATA) := rd
-          output(pipeline.data.RD_VALID) := True
+          when (!arbitration.isStalled) {
+            val rd = value(Data.CFIELD).mux(
+              FieldSelect.PERM -> cap.perms.asIsaBits.asUInt.resized,
+              FieldSelect.BASE -> cap.base,
+              FieldSelect.LEN -> cap.length,
+              FieldSelect.TAG -> cap.tag.asUInt.resized,
+              FieldSelect.OFFSET -> cap.offset,
+              FieldSelect.ADDR -> (cap.base + cap.offset) // TODO: use ALU
+            )
+
+            output(pipeline.data.RD_DATA) := rd
+            output(pipeline.data.RD_VALID) := True
+          }
         }
 
         when (value(Data.CMODIFY)) {
+          arbitration.rs1Needed := True
           val cs = value(context.data.CS1_DATA)
+
+          arbitration.rs2Needed := True
           val bounds = value(pipeline.data.RS2_DATA)
+
           val cd = Capability()
           cd := cs
 
-          switch (value(Data.CMODIFICATION)) {
-            is (Modification.SET_BOUNDS) {
-              val newTop = cs.address + bounds
+          when (!arbitration.isStalled) {
+            switch(value(Data.CMODIFICATION)) {
+              is(Modification.SET_BOUNDS) {
+                val newTop = cs.address + bounds
 
-              when (!cs.tag) {
-                // TODO: tag violation
-              } elsewhen (cs.address < cs.base) {
-                // TODO: length violation
-              } elsewhen (newTop > cs.top) {
-                // TODO: length violation
-              } otherwise {
-                cd.base := cs.address
-                cd.length := bounds
-                cd.offset := 0
+                when(!cs.tag) {
+                  // TODO: tag violation
+                } elsewhen (cs.address < cs.base) {
+                  // TODO: length violation
+                } elsewhen (newTop > cs.top) {
+                  // TODO: length violation
+                } otherwise {
+                  cd.base := cs.address
+                  cd.length := bounds
+                  cd.offset := 0
+                }
               }
             }
-          }
 
-          output(context.data.CD_DATA) := cd
-          output(pipeline.data.RD_VALID) := True
+            output(context.data.CD_DATA) := cd
+            output(pipeline.data.RD_VALID) := True
+          }
         }
       }
     }
