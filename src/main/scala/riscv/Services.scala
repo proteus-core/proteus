f85cc66779d68a2aabb9d5bf916e8520e141bdab
marton bognar <marton.bognar@cs.kuleuven.be>
2022-09-22 18:48:13 +0200

    Formatting

--- src/main/scala/riscv/Services.scala
+++ src/main/scala/riscv/Services.scala
@@ -306,7 +306,9 @@ trait JumpService {
 
 trait BranchTargetPredictorService {
   def predictedPc(stage: Stage): UInt
-  def predictionForAddress(address: UInt): UInt // TODO: should it be Flow[UInt] to signal no prediction instead of forcing + 4?
+  def predictionForAddress(
+      address: UInt
+  ): UInt // TODO: should it be Flow[UInt] to signal no prediction instead of forcing + 4?
   def setPredictedPc(stage: Stage, pc: UInt): Unit
 }
 
